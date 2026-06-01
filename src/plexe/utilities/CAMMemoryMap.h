dd96dbd5906b9f5204646c6b69b297d6763f1e95
Riccardo Modina <mod.riccardo03@gmail.com>
2026-06-01 11:49:58 +0200

    removed sender in CamMemoyMap since not used anymore to detect replay misbehaviour

--- src/plexe/utilities/CAMMemoryMap.h
+++ src/plexe/utilities/CAMMemoryMap.h
@@ -5,16 +5,11 @@
 
 namespace plexe {
 
-struct MessageInfo {
-    int sender;
-    double timestamp;
-};
-
 class CAMMemoryMap {
 private:
     // Key: payload hash (size_t)
-    // Value: insertion sender and timestamp (MessageInfo)
-    std::unordered_map<size_t, MessageInfo> memoryMap;
+    // Value: insertion timestamp (double)
+    std::unordered_map<size_t, double> memoryMap;
 
 public:
     CAMMemoryMap() = default;
@@ -33,21 +28,21 @@ public:
         return memoryMap.find(key) != memoryMap.end();
     }
 
-    // Add a key (hash) with its sender and insertion timestamp.
+    // Add a key (hash) with its insertion timestamp.
     // Returns true if added/updated successfully, false if already present (replay attack).
-    bool add(size_t key, int sender, double timestamp) {
+    bool add(size_t key, double timestamp) {
         auto it = memoryMap.find(key);
         if (it != memoryMap.end()) {
             return false; // attack: payload hash already present
         }
-        memoryMap[key] = {sender, timestamp};
+        memoryMap[key] = timestamp;
         return true;
     }
 
     // Garbage collection: remove all entries older than maxAgeCAMReplayDetection seconds relative to currentTime
     void garbageCollect(double currentTime, double maxAgeCAMReplayDetection) {
         for (auto it = memoryMap.begin(); it != memoryMap.end(); ) {
-            if ((currentTime - it->second.timestamp) > maxAgeCAMReplayDetection) {
+            if ((currentTime - it->second) > maxAgeCAMReplayDetection) {
                 it = memoryMap.erase(it);
             } else {
                 ++it;
